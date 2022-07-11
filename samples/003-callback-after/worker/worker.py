3e3440740a9e3409244b10291cfd41c54a09eb31
dennis5943 <dennis5943@gmail.com>
2022-07-12 00:38:47 +0800

    003

--- samples/003-callback-after/worker/worker.py
+++ samples/003-callback-after/worker/worker.py
@@ -12,23 +12,41 @@ def task_5(task):
         logs=['one','two','tree']
     )
 
-def task_6(task):
-    print('================================================')
-    print("callback after 60 sec",task)
-    
-    return ConductorWorker.task_result(
-        status=TaskStatus.IN_PROGRESS,
-        output= {"status":"Send OK","valAddOne":30},
-        logs=['one','two','tree'],
-        reasonForIncompletion='',
-        callbackAfterSeconds=10
-    )
+class Task6Worker(ConductorWorker):
+    def __init__(self, server_url, thread_count, polling_interval, worker_id=None):
+        super().__init__(server_url, thread_count, polling_interval, worker_id)
+
+        self.maxCount = 3
+        pass
+
+    def task_6(self,task):
+        print('================================================')
+        print("callback after 20 sec,remain counts:",self.maxCount)
+
+        if self.maxCount:
+            res = ConductorWorker.task_result(
+                    status=TaskStatus.IN_PROGRESS,
+                    output= {"status":"Send OK","valAddOne":30,"maxCount":self.maxCount},
+                    logs=['one','two','tree']
+                )
+            res['callbackAfterSeconds'] = 20
+            self.maxCount -= 1
+        else:
+            res = ConductorWorker.task_result(
+                    status=TaskStatus.COMPLETED,
+                    output= {"status":"Send OK","valAddOne":30,"maxCount":self.maxCount},
+                    logs=['one','two','tree']
+                )
+            res['callbackAfterSeconds'] = 20
+        return res
 
 def main():
     print('Starting Kitchensink workflows')
     cc = ConductorWorker('http://localhost:8080/api', 1, 0.1)
     cc.start('task_5', task_5, False)
-    cc.start('task_6', task_6, True)
+    
+    task6 = Task6Worker('http://localhost:8080/api', 1, 0.1)
+    task6.start('task_6', task6.task_6, True)
 
 if __name__ == '__main__':
     main()
